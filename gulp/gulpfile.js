6b3feb7d2124ea79a4ea5724a0a994cdcabac617
CX043212 <2754795092@qq.com>
2018-12-28 23:15:54 +0800

    首页布局

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -6,18 +6,23 @@ const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 gulp.task('js',function(){
-	gulp.src('src/js/*.js').pipe(concat('index.min.js'))
+	gulp.src('./src/js/*.js').pipe(concat('index.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('./dist/js'));
 })
 gulp.task('sass',()=>{
 	gulp.src('./src/sass/*.scss')
 	.pipe(sass())
 	.pipe(rename({"suffix" : ".min"}))
-	//.pipe(gulp.dest('./dist/css'));
+	.pipe(gulp.dest('./dist/css'));
+})
+gulp.task('img',function(){
+	gulp.src('./src/img/*.*').pipe(imagemin())
+	.pipe(gulp.dest('./dist/img'));
 })
 gulp.task('default',function(){
-	gulp.watch('./src/js/*.js',gulp.series('js'));
+	gulp.watch('./src/js/*.js',['js']);
 	gulp.watch('./src/sass/*.scss',['sass']);
+	gulp.watch('./src/sass/*.*',['img'])
 
 })
